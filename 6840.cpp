644e317d7aa4a5327018bc7165b969edb96a687c
JohnPark5 <phd123123@naver.com>
2024-07-04 00:24:36 +0900

    7/4 codes for solving BOJ

---
+++ 6840.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	
+	cin >> a >> b >> c;
+	
+	if(a < b) {
+		if(b < c)
+			cout << b << '\n';
+		else if(a < c)
+			cout << c << '\n';
+		else
+			cout << a << '\n';
+	}
+	else {
+		if(a < c) {
+			cout << a << '\n';
+		}
+		else if(b < c) {
+			cout << c << '\n';
+		}
+		else {
+			cout << b << '\n';
+		}
+	}
+	
+	
+	return 0;
+}
